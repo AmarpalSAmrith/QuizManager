5c2793ff12ef18744d9db6b51aa377a66a494076
AmarpalSAmrith <amarpal.amrith@valtech.com>
2020-06-22 11:58:17 +0100

    First Commit

---
+++ src/test/java/com/synoptic/project/quiz/manager/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synoptic.project.quiz.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
