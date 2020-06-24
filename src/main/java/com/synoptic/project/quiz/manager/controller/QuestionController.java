b7e820c83aa965d2f5efae43e6e166792e6fa434
AmarpalSAmrith <amarpal.amrith@valtech.com>
2020-06-24 19:03:02 +0100

    all quizzes setup. still error occuring

--- src/main/java/com/synoptic/project/quiz/manager/controller/QuestionController.java
+++ src/main/java/com/synoptic/project/quiz/manager/controller/QuestionController.java
@@ -1,8 +1,10 @@
 package com.synoptic.project.quiz.manager.controller;
 
+import com.synoptic.project.quiz.manager.model.Pagination;
 import com.synoptic.project.quiz.manager.model.Question;
 import com.synoptic.project.quiz.manager.model.Quiz;
 import com.synoptic.project.quiz.manager.service.QuestionService;
+import java.util.stream.IntStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,11 +14,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class QuestionController extends QuizManagerController<Question, Quiz> {
 
-  public static final String ROOT_FOLDER = "quizzes";
+  public static final String ROOT_FOLDER = "questions";
   private final QuestionService questionService;
 
   @Autowired
@@ -24,12 +27,22 @@ public class QuestionController extends QuizManagerController<Question, Quiz> {
     this.questionService = questionService;
   }
 
+  @GetMapping(ROOT_QUESTION)
+  public RedirectView getQuestionHomePage() {
+    return super.submitRedirect(ROOT_QUESTION + HOME_URL + Pagination.getDefaultURL());
+  }
+
   @GetMapping(ROOT_QUESTION + HOME_URL + "{pageNumber}/{pageSize}")
   public ModelAndView getAllQuestions(@PathVariable Integer pageNumber,
       @PathVariable Integer pageSize) {
     Pageable pageable = PageRequest.of(pageNumber, pageSize, Direction.ASC, "question");
     Page<Question> results = questionService.getAllQuestionsOrderedByQuestion(pageable);
-    ModelAndView model = new ModelAndView(ROOT_FOLDER);
+    ModelAndView model = new ModelAndView(ROOT_FOLDER + INDEX_VIEW_NAME);
+    model.addObject("answersIteration",
+        IntStream.range(0, 5)
+            .boxed()
+            .toArray(Integer[]::new));
+    results.toList().get(0).getAnswers().get(0);
     return super.getAllResults(model, ROOT_QUESTION, pageNumber, pageSize, results);
   }
 
