ef8fb2285b98cf865019f09cd9ee0e27edea72c1
AmarpalSAmrith <amarpal.amrith@valtech.com>
2020-06-25 20:02:02 +0100

    Altered Spring security to function. removed users data structure and introduced in memory authentication

--- src/main/java/com/synoptic/project/quiz/manager/configuration/SecurityConfiguration.java
+++ src/main/java/com/synoptic/project/quiz/manager/configuration/SecurityConfiguration.java
@@ -1,17 +1,12 @@
 package com.synoptic.project.quiz.manager.configuration;
 
 import com.synoptic.project.quiz.manager.handler.CustomAccessDeniedHandler;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -22,40 +17,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   private static final String ROLE_VIEW = "VIEW";
   private static final String ROLE_RESTRICTED = "RESTRICTED";
 
-  @Autowired
-  UserDetailsService userDetailsService;
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService);
-  }
-
-  private String[] getAcceptedUrls(final String role) {
-    // TODO: configure the allowed URLs
-    List<String> restrictedOnlyUrls = new ArrayList<>(Arrays.asList("/**/view/**", "/**/home/**"));
-    List<String> viewOnlyUrls = new ArrayList<>(
-        Arrays.asList("/user/**", "/quiz/**"));
-    List<String> editOnlyUrls = new ArrayList<>(
-        Arrays.asList("/admin", "/**/edit/**", "/**/new/**", "/**/update/**", "/**/delete/**"));
-    switch (role) {
-      case ROLE_RESTRICTED:
-        return restrictedOnlyUrls.toArray(new String[0]);
-      case ROLE_VIEW:
-        viewOnlyUrls.addAll(restrictedOnlyUrls);
-        return viewOnlyUrls.toArray(new String[0]);
-      case ROLE_EDIT:
-        editOnlyUrls.addAll(viewOnlyUrls);
-        editOnlyUrls.addAll(restrictedOnlyUrls);
-        return editOnlyUrls.toArray(new String[0]);
-    }
-    throw new IllegalArgumentException(role + " is not a recognised role");
+    auth.inMemoryAuthentication()
+        .withUser("editUser")
+        .password(getPasswordEncoder().encode("password"))
+        .roles(ROLE_EDIT);
+    auth.inMemoryAuthentication()
+        .withUser("viewUser")
+        .password(getPasswordEncoder().encode("password"))
+        .roles(ROLE_VIEW);
+    auth.inMemoryAuthentication()
+        .withUser("restrictedUser")
+        .password(getPasswordEncoder().encode("password"))
+        .roles(ROLE_RESTRICTED);
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    String[] restrictedOnlyUrls = {"/**/view/**", "/**/home/**"};
-    String[] viewOnlyUrls = {"/user/**", "/quiz/**"};
-    String[] editOnlyUrls = {"/admin", "/**/edit/**", "/**/new/**", "/**/update/**", "/**/delete/**"};
+    String[] restrictedOnlyUrls = {"/quiz/", "/**/home/**", "/**/view/**"};
+    String[] viewOnlyUrls = {"/question/**"};
+    String[] editOnlyUrls = {"/**/edit/**", "/**/new/**", "/**/update/**", "/**/delete/**", "/**/add"};
 
     http.csrf().disable()
         .authorizeRequests()
@@ -76,14 +59,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   }
 
 
-
   @Bean
   public PasswordEncoder getPasswordEncoder() {
-    return NoOpPasswordEncoder.getInstance();
+    return new BCryptPasswordEncoder();
   }
 
   @Bean
   public AccessDeniedHandler accessDeniedHandler() {
     return new CustomAccessDeniedHandler();
   }
+
 }
---
+++ src/test/java/com/synoptic/project/quiz/manager/service/QuizServiceTest.java
@@ -0,0 +1,7 @@
+package com.synoptic.project.quiz.manager.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuizServiceTest {
+
+}
